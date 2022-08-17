6a712d1d2f4b6162cdad78cef34c6f217215fc14
Jordan <jordanrob709@gmail.com>
2022-08-17 03:37:38 +0300

    setup project structure

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Business from "./Business"
+import Clients from "./Clients"
+import CTA from "./CTA"
+import Footer from "./Footer"
+import Hero from "./Hero"
+import Navbar from "./Navbar"
+import Stats from "./Stats"
+import Testimonial from "./Testimonial"
+import Billing from "./Billing"
+import CardDeal from "./CardDeal"
+
+export {
+ Navbar,
+ Stats,
+ Testimonial,
+ Hero,
+ Footer,
+ CTA,
+ Clients,
+ Business,
+ Billing,
+ CardDeal
+}
